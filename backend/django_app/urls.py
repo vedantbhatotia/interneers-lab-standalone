75241c7ec61f49c94f15211cd484c0feeeee4fdc
vedantbhatotia <vedantbhatotia@gmail.com>
2025-03-09 23:53:18 +0530

    Your descriptive commit message

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -1,9 +1,24 @@
+# from django.contrib import admin
+# from django.urls import path
+# from django.http import HttpResponse
+# from django.http import JsonResponse
+
+# def hello_world(request):
+#     name = request.GET.get("name","World")
+#     return JsonResponse("updated hello world function",name)
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('hello/', hello_world),
+# ]
+
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 def hello_world(request):
-    return HttpResponse("Hello, world! This is our interneers-lab Django server.")
+    name = request.GET.get("name", "World")
+    return JsonResponse({"message": f"updated hello world function, {name}"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
