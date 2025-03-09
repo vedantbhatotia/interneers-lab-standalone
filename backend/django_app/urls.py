9035f7ce0852efccf8babc2f73ecbb1f6d3b0d04
vedantbhatotia <vedantbhatotia@gmail.com>
2025-03-10 00:05:17 +0530

    update

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -12,7 +12,7 @@
 #     path('hello/', hello_world),
 # ]
 
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
 
