731d6e0697a82c90d79301f81115e74289842e7e
vedantbhatotia <vedantbhatotia@gmail.com>
2025-03-28 01:13:51 +0530

    use nested serializer representation

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
+from django.core.validators import MinValueValidator
 from .models import Product, ProductCategory
-from django.core.validators import MinValueValidator 
 
 class ProductCategorySerializer(serializers.Serializer):
     id = serializers.CharField(read_only=True)
@@ -9,21 +9,23 @@ class ProductCategorySerializer(serializers.Serializer):
     created_at = serializers.DateTimeField(read_only=True)
     updated_at = serializers.DateTimeField(read_only=True)
 
-    #convert the objectId type to string which can be serialized then 
+    # Convert the ObjectId to a string for proper JSON serialization.
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        # Convert the ObjectId to a string
         representation['id'] = str(instance.id)
         return representation
-
-
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.CharField(read_only=True)
     name = serializers.CharField(min_length=3)
     description = serializers.CharField()
-    category = serializers.PrimaryKeyRelatedField(queryset=ProductCategory.objects.all())
+    # Used for writing/updating the product
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=ProductCategory.objects.all(), write_only=True
+    )
     price = serializers.FloatField(validators=[MinValueValidator(0)])
+    # For output, return the full nested category object.
+    category_object = ProductCategorySerializer(read_only=True)
     brand = serializers.CharField(min_length=1)
     stock = serializers.IntegerField(validators=[MinValueValidator(0)])
     created_at = serializers.DateTimeField(read_only=True)
@@ -31,7 +33,7 @@ class ProductSerializer(serializers.Serializer):
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        # Convert the ObjectId to a string
         representation['id'] = str(instance.id)
-        representation['category'] = str(instance.category.id)
+        if representation.get('category_object'):
+            representation['category_object']['id'] = str(instance.category.id)
         return representation
