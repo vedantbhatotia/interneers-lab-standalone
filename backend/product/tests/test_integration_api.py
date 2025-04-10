447044076466a25973f25e1597fb281b91ce9431
vedantbhatotia <vedantbhatotia@gmail.com>
2025-04-11 00:32:51 +0530

    added integration tests for category api's

--- backend/product/tests/test_integration_api.py
+++ backend/product/tests/test_integration_api.py
@@ -15,3 +15,22 @@ def test_list_categories():
     assert isinstance(data,list)
     # assert any("TestCat" for cat.get("title","") 
     assert any("TestCat" in cat.get("title","") for cat in data)
+
+
+def test_create_category():
+    data = {
+        "title":"Integration Test Category",
+        "description":"Created via Integration test"
+    }
+    response = client.post("/api/categories/",data,format="json")
+    assert response.status_code == 200 or response.status_code == 201
+    data = response.json()
+    assert data["title"] == "Integration Test Category"
+
+
+def test_get_category_detail():
+    response = client.get("/api/categories/")
+    category_id = response.json()[0]["id"]
+    category_detail = client.get(f"/api/categories/{category_id}/")
+    assert category_detail.status_code == 200
+
