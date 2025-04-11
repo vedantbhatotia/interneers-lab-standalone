08916cf523953c9a5c6db8af865301f2c3c37137
vedantbhatotia <vedantbhatotia@gmail.com>
2025-04-11 21:46:53 +0530

    check hooks

--- backend/run_tests.bat
+++ backend/run_tests.bat
@@ -1,23 +1,19 @@
-
 @echo off
-REM Prevents the commands in the script from being printed to the command window as they are executed.
-
-
-
 REM --------------------------------------------------------
 REM run_tests.bat
-REM This batch file runs  pytest test suite.
+REM This batch file runs the pytest test suite.
 REM It stops on the first failure (--maxfail=1) and
 REM disables warnings (--disable-warnings) for a cleaner output.
 REM --------------------------------------------------------
 
+REM Change to the backend directory (where manage.py is)
+cd /d "%~dp0"
 
-REM Run pytest using Python. 
-python -m pytest --maxfail=1 --disable-warnings 
+REM Set the Django settings module
+set DJANGO_SETTINGS_MODULE=django_app.settings
 
+REM Add current directory to PYTHONPATH for Django to detect it
+set PYTHONPATH=%CD%
 
-@REM bat file contains series of commands that Windows will execute in the order they are written in the file
-
-@REM they are mainly used to automate repetitive tasks
-
-@REM in this running this script will execute the complete test_suite with all the additional commands
+REM Run pytest using Python
+python -m pytest --maxfail=1 --disable-warnings
