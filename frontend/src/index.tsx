c323cba1e16170e3c195a717d78422a5f3cbff4a
vedantbhatotia <vedantbhatotia@gmail.com>
2025-05-12 22:13:26 +0530

    Created ProductCard

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
+// import App from "./AppWithRouter";
+import App from "./AppWithApi";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
